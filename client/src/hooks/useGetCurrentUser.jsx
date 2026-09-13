f3da3ef5865b123bca92f8a8b347085b4df605ce
Arpit Singh <arpitchauhan1607@gmail.com>
2026-09-13 15:43:06 +0530

    Use bearer token for current user

--- client/src/hooks/useGetCurrentUser.jsx
+++ client/src/hooks/useGetCurrentUser.jsx
@@ -1,23 +1,39 @@
-import axios from 'axios'
-import React from 'react'
-import { useEffect } from 'react'
-import { serverUrl } from '../App'
-import { useDispatch } from 'react-redux'
-import { setUserData } from '../redux/userSlice'
+import axios from "axios"
+import { useEffect } from "react"
+import { serverUrl } from "../App"
+import { useDispatch } from "react-redux"
+import { setUserData } from "../redux/userSlice"
 
 function useGetCurrentUser() {
-    const dispatch=useDispatch()
+    const dispatch = useDispatch()
+
     useEffect(() => {
         const getCurrentUser = async () => {
             try {
-              const result=await axios.get(`${serverUrl}/api/user/me`,{withCredentials:true})
-              dispatch(setUserData(result.data))
+                const token = localStorage.getItem("nova_token")
+
+                const result = await axios.get(
+                    `${serverUrl}/api/user/me`,
+                    {
+                        withCredentials: true,
+                        headers: token
+                            ? {
+                                Authorization: `Bearer ${token}`
+                            }
+                            : {}
+                    }
+                )
+
+                dispatch(setUserData(result.data))
             } catch (error) {
-console.log(error)
+                console.log(error)
             }
         }
+
         getCurrentUser()
-    }, [])
+    }, [dispatch])
+
+    return null
 }
 
 export default useGetCurrentUser
