1631c21956619201a50a2f0fc677dee57385f1e8
Arpit Singh <arpitchauhan1607@gmail.com>
2026-09-13 14:40:20 +0530

    Prepare server for production

--- server/index.js
+++ server/index.js
@@ -10,23 +10,32 @@ import websiteRouter from "./routes/website.routes.js"
 import billingRouter from "./routes/billing.routes.js"
 import { stripeWebhook } from "./controllers/stripeWebhook.controller.js"
 
-const app=express()
+const app = express()
 
-app.post("/api/stripe/webhook",express.raw({type:"application/json"}),stripeWebhook)
-const port=process.env.PORT || 5000
+app.post(
+    "/api/stripe/webhook",
+    express.raw({ type: "application/json" }),
+    stripeWebhook
+)
+
+const port = process.env.PORT || 5000
+
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use("/api/website",websiteRouter)
-app.use("/api/billing",billingRouter)
 
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        credentials: true
+    })
+)
 
-app.listen(port,()=>{
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
+app.use("/api/website", websiteRouter)
+app.use("/api/billing", billingRouter)
+
+app.listen(port, () => {
     console.log("server started")
     connectDb()
 })
